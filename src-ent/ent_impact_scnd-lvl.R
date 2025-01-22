66c760ce827501efe2ddffc2be33c151ea8367e9
kel-github <getkellygarner@gmail.com>
2025-01-22 13:39:50 +1100

    "added correlations"

--- src-ent/ent_impact_scnd-lvl.R
+++ src-ent/ent_impact_scnd-lvl.R
@@ -7,6 +7,7 @@
 # clear environment and get what you need
 rm(list=ls())
 library(tidyverse)
+library(GGally)
 source(paste('src-ent/ent_functions.R', sep='/'))
 
 #################################################
@@ -72,4 +73,31 @@ with(flt, t.test(mu_flt ~ train_type))
 with(flt, t.test(scs_flt ~ train_type))
 with(flt, t.test(cntx_flt ~ train_type))
 
+#################################################
+# now, correlate the regressors with each other
+# and with the routine score
+# first get the routine score
+R <- read.csv(file=paste("../doors-data/data-wrangled/exp", exp_str, 
+                            "rscore-full.csv", sep="_"))
+# first get an average routine score
+R <- R %>% group_by(sub) %>% summarise(mur = mean(r))
+flt <- inner_join(flt, R, by="sub")
+# remove rows with nas
+flt <- flt %>% na.omit()
 
+# first, check for multivariate outliers
+mhl.mat <- as.matrix(flt[,c("mu_flt", "scs_flt", "cntx_flt", "mur")]) 
+mhl.cov <- cov(mhl.mat) # here I get the covariance matrix
+mhl.dist <- mahalanobis(mhl.mat, colMeans(mhl.mat), mhl.cov) # now calc the M dist
+hist(mhl.dist, breaks = 20)
+sprintf("For a Mahalanobis to be less that .1 per cent likely to have occured by chance, given our degrees of feedom (%f), it has to be a value greater than %f", length(mhl.dist)-1, qchisq(.001, df=length(mhl.dist)-1))
+
+#### learning transfer - we therefore remove the participants with a mahalanobis distance
+#### greater than 56.47
+rm_sbs <- as.numeric(names(mhl.dist)[which(mhl.dist > qchisq(.001, df=length(mhl.dist)-1))])
+flt <- flt %>% filter(!sub %in% rm_sbs)
+flt <- flt %>% mutate(mur_l = log(mur))
+ggpairs(flt %>% select(mu_flt, scs_flt, cntx_flt, mur_l),
+        mapping=ggplot2::aes(colour = flt$train_type),
+        upper = list(continuous = wrap("cor", method = "spearman")))
+### now do a pairs plot
